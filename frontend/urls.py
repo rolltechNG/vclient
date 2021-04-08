4b6fa2469fff7aef69b19aafe132749fc90d6eb5
Toluwalemi <toluwalemisrael@gmail.com>
2021-04-09 00:19:12 +0100

    feature: add view function and url for nin

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from . import views
 from .views import Home
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
+    path('verify/nin/', views.post_nin, name='post_nin')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,17 @@
+# Create your views here.
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from frontend.forms import NINPostForm
 
-# Create your views here.
 
 class Home(TemplateView):
     template_name = "hello-world.html"
+
+
+def post_nin(request):
+    form = NINPostForm()
+    return render(request,
+                  'ninform.html',
+                  {'form': form}
+                  )
