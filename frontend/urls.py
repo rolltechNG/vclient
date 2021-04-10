3f0add9adcc035c712b36be7eedffa0d62be1c58
Toluwalemi <toluwalemisrael@gmail.com>
2021-04-10 02:05:55 +0100

    feature: update views to handle nin api call

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 from .views import Home
 
-app_name = 'pages'
+app_name = 'frontend'
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,9 @@
 # Create your views here.
+import json
+
+import requests
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 from frontend.forms import NINPostForm
@@ -10,8 +14,22 @@ class Home(TemplateView):
 
 
 def post_nin(request):
-    form = NINPostForm()
-    return render(request,
-                  'ninform.html',
-                  {'form': form}
-                  )
+    if request.method == 'POST':
+        form = NINPostForm(request.POST)
+        if form.is_valid():
+            nin = form.cleaned_data['nin']
+            url = request.build_absolute_uri(reverse_lazy('nin_verification',
+                                                          kwargs={'token': 14499009348927979530087,
+                                                                  'nin': nin},
+                                                          ))
+            r = requests.get(url=url)
+            resp = r.text
+            final_resp = json.loads(resp)
+            resp_list = final_resp['data']
+
+            return render(request, 'ninsuccess.html', {'resp': resp_list})
+    else:
+        form = NINPostForm()
+    context = {'form': form, }
+
+    return render(request, 'ninform.html', context)
