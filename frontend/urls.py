ffc2718210db3e2705003349b97a3fdb0354673b
Toluwalemi <toluwalemisrael@gmail.com>
2021-04-15 12:59:25 +0100

    feature: add endpoint to test payment feature

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from . import views
-from .views import Home
+from .views import Home, TestPayment
 
 app_name = 'frontend'
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
+    path('paystack/', TestPayment.as_view(), name="home"),
     path('verify/nin/', views.post_nin, name='post_nin'),
     path('verify/phone/', views.post_phone, name='post_phone'),
     path('verify/demo/', views.post_demo, name='post_demo'),
