e38ec374ab84193b4589f02ce61d095b5f591b6b
Toluwalemi <toluwalemisrael@gmail.com>
2021-04-15 12:32:19 +0100

    feature: add price to djangoAdmin

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from frontend.models import Price
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ['nin_price', 'demo_price', 'phone_price']
